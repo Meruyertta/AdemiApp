a99df9aed01af759d222bf058e6b9e6b7a1f72ee
Meruyertta <tmeruert00@gmail.com>
2022-05-13 18:25:49 +0600

    Made buttons 'Do not have an account?' clickable :)

--- app/src/main/java/com/example/musicapp/LoginActivity.java
+++ app/src/main/java/com/example/musicapp/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.musicapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    public void register(View view) {
+        Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/example/musicapp/MainActivity.java
+++ app/src/main/java/com/example/musicapp/MainActivity.java
@@ -23,4 +23,5 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
         startActivity(intent);
     }
+
 }

f90cba5921bd007fd888da774da8dbd481d4a111
Meruyertta <tmeruert00@gmail.com>
2022-05-14 16:52:16 +0600

    Registration and login|Connected with Firebase

---
+++ app/src/main/java/com/example/musicapp/User.java
@@ -0,0 +1,14 @@
+package com.example.musicapp;
+
+public class User {
+    public String name;
+    public String email;
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public User() {
+    }
+}

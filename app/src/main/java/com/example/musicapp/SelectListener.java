49ec973b8c9702734fe373f98832497763a578e5
Meruyertta <tmeruert00@gmail.com>
2022-05-19 11:49:48 +0600

    select  listener

---
+++ app/src/main/java/com/example/musicapp/SelectListener.java
@@ -0,0 +1,10 @@
+package com.example.musicapp;
+
+import com.example.musicapp.recycleviews.ModelCategoryFragment;
+import com.example.musicapp.recycleviews.shoppingCart.ModelCartFragment;
+
+public interface SelectListener {
+    void onItemClicked(ModelCategoryFragment modelCategory);
+
+
+}
